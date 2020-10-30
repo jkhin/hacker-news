bfe8efba63d1c7140898cbe753b23cc704c9d5ed
Joe <jkhin.cb@gmail.com>
2020-10-30 16:44:10 -0500

    fixing some di definitions
    Signed-off-by: Joe <jkhin.cb@gmail.com>

--- app/src/main/kotlin/io/jk/dev/reign/hackernews/features/di/NewsModule.kt
+++ app/src/main/kotlin/io/jk/dev/reign/hackernews/features/di/NewsModule.kt
@@ -2,33 +2,35 @@ package io.jk.dev.reign.hackernews.features.di
 
 import io.jk.dev.reign.hackernews.core.network.core.NAMED_HACKER_NEWS_API
 import io.jk.dev.reign.hackernews.features.news.data.datasource.database.NewsDataStore
-import io.jk.dev.reign.hackernews.features.news.data.datasource.database.RemovedNewsDataStore
 import io.jk.dev.reign.hackernews.features.news.data.datasource.rest.NewsCloudStore
 import io.jk.dev.reign.hackernews.features.news.data.mapper.NewsEntityMapper
 import io.jk.dev.reign.hackernews.features.news.data.mapper.NewsMapper
+import io.jk.dev.reign.hackernews.features.news.data.repository.NewsRepositoryImpl
+import io.jk.dev.reign.hackernews.features.news.domain.interactor.NewsUseCase
+import io.jk.dev.reign.hackernews.features.news.domain.repository.NewsRepository
+import io.jk.dev.reign.hackernews.features.news.presentation.model.mapper.NewsModelMapper
+import io.jk.dev.reign.hackernews.features.news.presentation.viewmodel.NewsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 internal val newsFeatureModule by lazy {
     module {
 
-        single { NewsMapper() }
+        viewModel { NewsViewModel(get(), get()) }
+
+        single { NewsModelMapper() }
+
+        single { NewsUseCase(get()) }
+
         single { NewsEntityMapper() }
+        single { NewsMapper() }
 
         single { NewsCloudStore(get(named(NAMED_HACKER_NEWS_API))) }
 
         single { NewsDataStore() }
-        single { RemovedNewsDataStore() }
 
-//        viewModel { NewsViewModel(get(), get()) }
+        single<NewsRepository> { NewsRepositoryImpl(get(), get(), get(), get()) }
 
-//        single { TermsMapper() }
-//        single { TermsModelMapper() }
-//
-//        single { GetTermsUseCase(get()) }
-//
-//        single<TermsRepository> { TermsRepositoryImpl(get(), get()) }
-//
-//        single { TermsLocalStorage() }
     }
 }
